22c47d1690e25f55b8fb6e4380218f95380203fa
Aaron Smith <smitham50@gmail.com>
2019-03-24 18:01:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 function dwarfRollCall(dwarves) {
+  var list
+  for (var i = 0; i < dwarves.length; i++) {
+    if (i === 0) {
+      list = (i + 1) + '. ' + dwarves[i] + ' '
+    }
+    else {
+      list = list + (i + 1) + '. ' + dwarves[i] + ' '
+    }
+  }
+  return list
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var calls = []
+  for (var i = 0; i < calls.length; i++) {
+      calls.push(planeteerCalls[i] + '!')
+    }
+    calls.toUpperCase
+  return calls
 }
 
 function longPlaneteerCalls(words) {
